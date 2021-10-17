b3ee1f10216f9b640d4cc725277ca418ffc66199
alinakhanSystemsltd <90306505+alinakhanSystemsltd@users.noreply.github.com>
2021-10-17 16:25:22 +0500

    Update helloAlina.groovy

--- vars/helloAlina.groovy
+++ vars/helloAlina.groovy
@@ -1,3 +1,8 @@
 def call(String name, String dayOfWeek) {
     sh "echo Hello Alina ${name}. It is ${dayOfWeek}."
 }
+
+
+def call(Map config = [:]) {
+  sh "echo Hello Alina ${config.name}. It is ${config.dayOfWeek}."
+}
